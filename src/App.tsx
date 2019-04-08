7e74c319334ff03e202e4a4e64cce2c8e7726f3d
Fredrik Johansson <fredrik.johansson@thnx.se>
2019-04-08 13:31:44 +0200

    Update demo code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React, { useContext, useCallback, useMemo } from 'react'
+import React, { useCallback, useContext, useMemo } from 'react'
 import { MyContext } from './contexts/MyContext'
 
 const App = () => {
-  const [state, actions] = useContext(MyContext)
+    const [state, actions] = useContext(MyContext)
 
-  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => actions.changeValue(+event.target.value), [])
-  const value = useMemo(() => (state.isLoading ? '...' : state.value), [state.isLoading, state.value])
-  return (
-    <>
-      <div>{value}</div>
-      <button disabled={state.isLoading} onClick={actions.increment}>
-        Increment
-      </button>
-      <button disabled={state.isLoading} onClick={actions.reset}>
-        Clear
-      </button>
-      <button disabled={state.isLoading} onClick={actions.fetchDataAsync}>
-        Fetch
-      </button>
-      <input disabled={state.isLoading} value={value} onChange={handleChange} />
-      {state.isLoading && <div>is loading</div>}
-    </>
-  )
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => actions.changeValue(+event.target.value), [])
+    const value = useMemo(() => (state.isLoading ? '...' : state.value), [state.isLoading, state.value])
+    return (
+        <>
+            <div>{value}</div>
+            <button disabled={state.isLoading} onClick={actions.increment}>
+                Increment
+            </button>
+            <button disabled={state.isLoading} onClick={actions.reset}>
+                Clear
+            </button>
+            <button disabled={state.isLoading} onClick={actions.fetchDataAsync}>
+                Fetch
+            </button>
+            <input disabled={state.isLoading} value={value} onChange={handleChange} />
+            {state.isLoading && <div>is loading</div>}
+        </>
+    )
 }
 
 export default App
--- src/contexts/MyContext.tsx
+++ src/contexts/MyContext.tsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState, useCallback, useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import * as api from '../services/api'
 
 const MyContext = React.createContext({} as [
-  { isLoading: boolean; value: number },
-  { fetchDataAsync: () => void; increment: () => void; reset: () => void; changeValue: (value: number) => void }
+    { isLoading: boolean; value: number },
+    { fetchDataAsync: () => void; increment: () => void; reset: () => void; changeValue: (value: number) => void }
 ])
 
 const MyContextProvider: React.FunctionComponent = ({ children }) => {
-  const [isLoading, setLoading] = useState(true)
-  const [value, setValue] = useState(0)
-  const controller = useMemo(() => new AbortController(), [])
+    const [isLoading, setLoading] = useState(true)
+    const [value, setValue] = useState(0)
+    const controller = useMemo(() => new AbortController(), [])
 
-  const fetchDataAsync = useCallback(async () => {
-    setLoading(true)
-    const { signal } = controller
-    const { data, length } = await api.fetchRandomNumberAsync(signal)
-    if (length > 0) {
-      setValue(data[0])
+    const fetchDataAsync = useCallback(async () => {
+        setLoading(true)
+        const { signal } = controller
+        const { data, length } = await api.fetchRandomNumberAsync(signal)
+        if (length > 0) {
+            setValue(data[0])
+        }
+        setLoading(false)
+    }, [])
+
+    const actions = {
+        fetchDataAsync,
+        increment: useCallback(() => setValue(value + 1), [value]),
+        reset: useCallback(() => setValue(0), []),
+        changeValue: useCallback((value: number) => !isNaN(value) && setValue(value), []),
     }
-    setLoading(false)
-  }, [])
 
-  const actions = {
-    fetchDataAsync,
-    increment: useCallback(() => setValue(value + 1), [value]),
-    reset: useCallback(() => setValue(0), []),
-    changeValue: useCallback((value: number) => !isNaN(value) && setValue(value), [])
-  }
+    const state = { isLoading, value }
 
-  const state = { isLoading, value }
+    useEffect(() => {
+        fetchDataAsync()
+        return () => controller.abort()
+    }, [])
 
-  useEffect(() => {
-    fetchDataAsync()
-    return () => controller.abort()
-  }, [])
-
-  return <MyContext.Provider value={[state, actions]}>{children}</MyContext.Provider>
+    return <MyContext.Provider value={[state, actions]}>{children}</MyContext.Provider>
 }
 
 const MyContextConsumer = MyContext.Consumer
