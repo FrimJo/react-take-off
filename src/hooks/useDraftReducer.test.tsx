0976858bf3541f7bae868d609370712942e04d37
Fredrik Johansson <fredrik.johansson@thnx.se>
2019-06-29 21:43:17 +0200

    Update tests using @testing-library/react-hooks

--- src/hooks/useDraftReducer.test.tsx
+++ src/hooks/useDraftReducer.test.tsx
@@ -1,104 +1,58 @@
-import React from 'react'
-import { cleanup, fireEvent, render } from 'react-testing-library'
+import { act, renderHook } from '@testing-library/react-hooks'
 import exampleReducer, { Action, State } from 'reducers/exampleReducer'
 import useDraftReducer, { DraftReducer } from './useDraftReducer'
 
-afterEach(cleanup)
-
-const initialState = { name: '', age: 0 }
-
-const testDraftReducer: DraftReducer<State, Action> = (prevState, action) =>
-  action.draft
-
-const TestComponent: React.FunctionComponent<{
-  name: string
-  age: number
-  draftReducer: DraftReducer<State, Action>
-}> = ({ name, age, draftReducer }) => {
-  const [value, dispatch] = useDraftReducer(
-    exampleReducer,
-    initialState,
-    draftReducer
-  )
-  return (
-    <div>
-      <p data-testid="nameValue">{value.name}</p>
-      <p data-testid="ageValue">{value.age}</p>
-      <button
-        data-testid="setNameButton"
-        type="button"
-        onClick={() => dispatch({ type: 'SET_NAME', name })}
-      />
-      <button
-        data-testid="setAgeButton"
-        type="button"
-        onClick={() => dispatch({ type: 'SET_AGE', age })}
-      />
-    </div>
-  )
-}
-
 describe('useDraftReducer()', () => {
-  test('it renders', () => {
-    render(
-      <TestComponent name="Lisa" age={5} draftReducer={testDraftReducer} />
+  test('it runs', () => {
+    const draftReducer: DraftReducer<State, Action> = (prevState, action) =>
+      action.draft
+    const initialState: State = { name: '', age: 0 }
+    renderHook(() =>
+      useDraftReducer(exampleReducer, initialState, draftReducer)
     )
   })
 
-  test('has correct default values', () => {
-    const { getByTestId } = render(
-      <TestComponent name="Lisa" age={5} draftReducer={testDraftReducer} />
+  test('it runs width deafult state', () => {
+    const draftReducer: DraftReducer<State, Action> = (prevState, action) =>
+      action.draft
+    const initialState: State = { name: '', age: 0 }
+    const { result } = renderHook(() =>
+      useDraftReducer(exampleReducer, initialState, draftReducer)
     )
-    const nameValue = getByTestId('nameValue').textContent
-    expect(nameValue).toBe(initialState.name)
-    const ageValue = getByTestId('ageValue').textContent
-    expect(+(ageValue as string)).toBe(initialState.age)
+
+    const [value] = result.current
+    expect(value.name).toBe('')
+    expect(value.age).toBe(0)
   })
 
-  test('sets correct name on set name button click', async () => {
-    const newName = 'Lisa'
-    const { getByTestId } = render(
-      <TestComponent name={newName} age={5} draftReducer={testDraftReducer} />
+  test('sets name to Lisa on SET_NAME dispatch', () => {
+    const draftReducer: DraftReducer<State, Action> = (prevState, action) =>
+      action.draft
+    const initialState: State = { name: '', age: 0 }
+    const { result } = renderHook(() =>
+      useDraftReducer(exampleReducer, initialState, draftReducer)
     )
-    const setNameButton = getByTestId('setNameButton')
-    fireEvent.click(setNameButton)
-    const nameValue = getByTestId('nameValue').textContent
-    expect(nameValue).toBe(newName)
+
+    const [, dispatch] = result.current
+
+    const name = 'Lisa'
+    act(() => dispatch({ type: 'SET_NAME', name }))
+    const [value] = result.current
+    expect(value.name).toBe(name)
   })
 
-  test('sets correct age on set age button click', async () => {
-    const newAge = 5
-    const { getByTestId } = render(
-      <TestComponent
-        name={'Lisa'}
-        age={newAge}
-        draftReducer={testDraftReducer}
-      />
+  test('sets age to 7 on SET_AGE dispatch', () => {
+    const draftReducer: DraftReducer<State, Action> = (prevState, action) =>
+      action.draft
+    const initialState: State = { name: '', age: 0 }
+    const { result } = renderHook(() =>
+      useDraftReducer(exampleReducer, initialState, draftReducer)
     )
-    const setNameButton = getByTestId('setAgeButton')
-    fireEvent.click(setNameButton)
-    const ageValue = getByTestId('ageValue').textContent
-    expect(+(ageValue as string)).toBe(newAge)
-  })
+    const [, dispatch] = result.current
 
-  test('sets correct age on set age button click using draft reducer', async () => {
-    const newAge = 5
-    const draftAge = 24
-    const { getByTestId } = render(
-      <TestComponent
-        name={'Lisa'}
-        age={newAge}
-        draftReducer={(prevState, action) => {
-          if (action.type === 'SET_AGE') {
-            return { ...prevState, age: draftAge }
-          }
-          return action.draft
-        }}
-      />
-    )
-    const setNameButton = getByTestId('setAgeButton')
-    fireEvent.click(setNameButton)
-    const ageValue = getByTestId('ageValue').textContent
-    expect(+(ageValue as string)).toBe(draftAge)
+    const age = 7
+    act(() => dispatch({ type: 'SET_AGE', age }))
+    const [value] = result.current
+    expect(value.age).toBe(age)
   })
 })
