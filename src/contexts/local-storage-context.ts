545104c97a991781227880cb2847fb5e7764b3be
Fredrik Johansson <fredrik.johansson@thnx.se>
2020-04-14 14:32:52 +0200

    Reafactor local storage context to use same object instead of multiple

--- src/contexts/local-storage-context.ts
+++ src/contexts/local-storage-context.ts
@@ -2,42 +2,38 @@
 import React from 'react'
 import { buildContext } from 'utilities/build-context'
 
+type LocalStorgateType = { [key in string]: any }
+const LOCAL_STORAGE_KEY = 'reactTakeOff'
+
 export function useLocalStorageContext() {
-  const [storedValues, setStoredValues] = React.useState(() => {
-    const items: Array<{ [key in string]: any }> = []
-
-    for (let index = 0; index < window.localStorage.length; index++) {
-      const key = window.localStorage.key(index)
-      if (key == null) {
-        continue
-      }
-      try {
-        const itemJSON = window.localStorage.getItem(key)
-        const value = itemJSON == null ? itemJSON : JSON.parse(itemJSON)
-        items.push({ [key]: value })
-      } catch (error) {
-        continue
-      }
-    }
-
-    return Object.assign({}, ...items)
+  const [storedValues, setStoredValues] = React.useState<LocalStorgateType>(() => {
+    const itemJSON = window.localStorage.getItem(LOCAL_STORAGE_KEY)
+    return itemJSON == null ? {} : JSON.parse(itemJSON)
   })
 
   const setValue = React.useCallback(
-    (key: string, value: any) => {
+    function <ValueType>(key: string, value: ((prevValue: ValueType) => ValueType) | ValueType) {
       // Allow value to be a function so we have same API as useState
       const valueToStore = value instanceof Function ? value(storedValues[key]) : value
+
       // Save state
-      setStoredValues((prevValues: any) => ({ ...prevValues, [key]: value }))
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      setStoredValues((prevValues) => {
+        const updatedValues = { ...prevValues, [key]: valueToStore }
+        // Save to local storage
+        window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedValues))
+        return updatedValues
+      })
     },
     [storedValues]
   )
 
   const clearValue = (key: string) => {
-    setStoredValues((prevValues: any) => ({ ...prevValues, [key]: null }))
-    window.localStorage.removeItem(key)
+    setStoredValues((prevValues: any) => {
+      const values = { ...prevValues }
+      delete values[key]
+      window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values))
+      return values
+    })
   }
 
   return { state: { storedValues }, actions: { setValue, clearValue } }
