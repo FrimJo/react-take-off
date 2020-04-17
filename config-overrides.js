c8a075d5591592a857aefe92666916fce6d4c937
Fredrik Johansson <fredrik.johansson@thnx.se>
2020-04-17 23:26:20 +0200

    Add explanatory comments

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,6 @@ const { override, useEslintRc } = require('customize-cra')
 const { addReactRefresh } = require('customize-cra-react-refresh')
 
 module.exports = override(
-  useEslintRc(path.resolve(__dirname, '.eslintrc.json')),
-  addReactRefresh({ disableRefreshCheck: true })
+  useEslintRc(path.resolve(__dirname, '.eslintrc.json')), // Makes sure we use local eslintrc-file, same as EXTEND_ESLINT=true in .env
+  addReactRefresh({ disableRefreshCheck: true }) // Enables react-refresh
 )
