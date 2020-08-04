645d575bd295c44133a043f1e1ab11d74bae3590
Fredrik Johansson <fredrik.johansson@thnx.se>
2020-08-05 01:42:47 +0200

    Refactor with clearer logic

--- src/utilities/get-names-for-object.ts
+++ src/utilities/get-names-for-object.ts
@@ -3,20 +3,18 @@ import { Name } from 'types'
 export function getNamesForObject<T extends Object>(parent: T, parentStr = ''): Name<T> {
   const returnObj = {}
 
-  for (let key in parent as Object) {
+  for (const key in parent as Object) {
     if (!parent.hasOwnProperty(key)) continue
+
     const child = parent[key]
     const childStr = parentStr.length === 0 ? key : `${parentStr}.${key}`
-    if (child instanceof Object) {
-      if (Object.keys(child).length === 0) {
-        returnObj[key] = childStr
-      } else {
-        returnObj[key] = getNamesForObject(child, childStr)
-      }
+
+    if (child instanceof Object && Object.keys(child).length > 0) {
+      returnObj[key] = getNamesForObject(child, childStr)
     } else {
       returnObj[key] = childStr
     }
   }
 
-  return (returnObj as unknown) as Name<T>
+  return returnObj as Name<T>
 }
